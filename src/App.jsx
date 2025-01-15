e274c7e69a642d661c75756f3676b8fdb0aa6985
Abir Chodha <abirchodha1@gmail.com>
2025-01-15 16:53:16 +0530

    Add prettier tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl p-3  font-bold  underline">Hello world!</h1>;
+  return (
+    <h1 className="bg-red-200 p-3 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
 }
