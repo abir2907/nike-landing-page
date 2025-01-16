376c19722d81af3b462bc9c9bf4b51c774002663
Abir Chodha <abirchodha1@gmail.com>
2025-01-16 21:03:07 +0530

    Styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,23 +13,23 @@ import Nav from "./components/Nav";
 const App = () => (
   <main className="max-container relative">
     <Nav />
-    <section className="xl:padding-l wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b max-container">
       <Hero />
     </section>
-    <section className="padding">
+    <section className="padding max-container">
       <PopularProducts />
     </section>
-    <section className="padding">
+    <section className="padding max-container">
       <SuperQuality />
     </section>
-    <section className="padding-x py-10">
+    <section className="padding-x max-container py-10">
       <Services />
     </section>
-    <section className="padding">
+    <section className="padding max-container">
       <SpecialOffer />
     </section>
     <section className="padding bg-pale-blue">
-      <CustomerReviews />
+      <CustomerReviews className="w-full" />
     </section>
     <section className="padding-x w-full py-16 sm:py-32">
       <Subscribe />
